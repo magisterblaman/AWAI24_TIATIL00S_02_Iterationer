377c16eb576da40cd1cd23f8844ae178d6d0c7c8
magisterblaman <hannes.blaman@lbs.se>
2024-09-03 10:16:43 +0200

    While loop

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from turtle import *
 
 speed(1)
 
+forward(100)
+penup()
+right(180)
+forward(20)
+pendown()
+left(135)
+forward(20)
+penup()
+back(20)
+pendown()
+left(90)
+forward(20)
+back(20)
 right(45)
-
-while distance(0, 0) < 200:
-    forward(20)
-    left(90)
-    forward(20)
-    right(90)
+penup()
+back(80)
 
 done()
